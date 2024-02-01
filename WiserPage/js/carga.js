ae8d5771b2a8024bc2d73bf32918d74bf411c7e8
Wiser <wiserepic3@gmail.com>
2024-02-01 16:41:01 -0600

    FIX problema con la pantalla de carga, ahora se elimina al finalizar

--- WiserPage/js/carga.js
+++ WiserPage/js/carga.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Obtener la referencia al elemento loader
+    var loader = document.getElementById('loader');
+    
     // Ocultar la pantalla de carga después de 1.3 segundos
     setTimeout(function() {
-        document.getElementById('loader').style.opacity = '0';
+        loader.style.opacity = '0';
         document.getElementById('content').style.display = 'block';
+        
+        // Eliminar el elemento loader del DOM después de la transición de opacidad
+        setTimeout(function() {
+            loader.remove();
+        }, 1200);
     }, 1100);
 });
